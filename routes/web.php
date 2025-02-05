a97386b71daa5a4223ecf262ff9ebaa74aec703a
frashsym <rfqfrashsym@gmail.com>
2025-02-05 21:15:48 +0700

    push template

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/index', function () {
+    return view('index.index');
+});
+
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
 require __DIR__.'/auth.php';
