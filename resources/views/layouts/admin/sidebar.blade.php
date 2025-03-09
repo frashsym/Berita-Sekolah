af78fb9123bb16a106033409eca22f64cdcbb1ad
frashsym <rfqfrashsym@gmail.com>
2025-03-10 06:59:27 +0700

    icon

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -33,33 +33,21 @@
            <!-- Nav Item - Berita -->
            <li class="nav-item {{ request()->is('berita') ? 'active' : '' }}">
                <a class="nav-link" href="{{ url('/berita') }}">
-                   <svg style="width: 15px; height: 13px;" xmlns="http://www.w3.org/2000/svg"
-                       viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
-                       <path fill="#ffffff"
-                           d="M96 96c0-35.3 28.7-64 64-64l288 0c35.3 0 64 28.7 64 64l0 320c0 35.3-28.7 64-64 64L80 480c-44.2 0-80-35.8-80-80L0 128c0-17.7 14.3-32 32-32s32 14.3 32 32l0 272c0 8.8 7.2 16 16 16s16-7.2 16-16L96 96zm64 24l0 80c0 13.3 10.7 24 24 24l112 0c13.3 0 24-10.7 24-24l0-80c0-13.3-10.7-24-24-24L184 96c-13.3 0-24 10.7-24 24zm208-8c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l48 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-48 0c-8.8 0-16 7.2-16 16zM160 304c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16zm0 96c0 8.8 7.2 16 16 16l256 0c8.8 0 16-7.2 16-16s-7.2-16-16-16l-256 0c-8.8 0-16 7.2-16 16z" />
-                   </svg>
+                   <i class="fas fa-fw fa-newspaper"></i>
                    <span class="ml-1">Berita</span></a>
            </li>
 
            <!-- Nav Item - Kategori -->
            <li class="nav-item {{ request()->is('kategori') ? 'active' : '' }}">
                <a class="nav-link" href="{{ url('/kategori') }}">
-                   <svg style="width: 15px; height: 13px;" xmlns="http://www.w3.org/2000/svg"
-                       viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
-                       <path fill="#ffffff"
-                           d="M40 48C26.7 48 16 58.7 16 72l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24L40 48zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32L192 64zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32l288 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-288 0zM16 232l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24l0 48c0 13.3 10.7 24 24 24l48 0c13.3 0 24-10.7 24-24l0-48c0-13.3-10.7-24-24-24l-48 0z" />
-                   </svg>
+                   <i class="fas fa-fw fa-list"></i>
                    <span class="ml-1">Kategori</span></a>
            </li>
 
            <!-- Nav Item - Comment -->
            <li class="nav-item {{ request()->is('comments') ? 'active' : '' }}">
                <a class="nav-link" href="{{ url('/comments') }}">
-                   <svg style="width: 15px; height: 13px;" xmlns="http://www.w3.org/2000/svg"
-                       viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
-                       <path fill="#ffffff"
-                           d="M512 240c0 114.9-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6C73.6 471.1 44.7 480 16 480c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c0 0 0 0 0 0s0 0 0 0s0 0 0 0c0 0 0 0 0 0l.3-.3c.3-.3 .7-.7 1.3-1.4c1.1-1.2 2.8-3.1 4.9-5.7c4.1-5 9.6-12.4 15.2-21.6c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208z" />
-                   </svg>
+                   <i class="fas fa-fw fa-comments"></i>
                    <span class="ml-1">Komentar</span></a>
            </li>
 
@@ -67,21 +55,14 @@
            @if (auth()->user() && auth()->user()->isSuperAdmin())
                <li class="nav-item {{ request()->is('admin') ? 'active' : '' }}">
                    <a class="nav-link" href="{{ url('/admin') }}">
-                       <svg style="width: 15px; height: 13px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                           <path fill="#ffffff"
-                               d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512l388.6 0c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304l-91.4 0z" />
-                       </svg>
+                       <i class="fas fa-user-shield"></i>
                        <span class="ml-1">Admin</span>
                    </a>
                </li>
 
                <li class="nav-item {{ request()->is('role') ? 'active' : '' }}">
                    <a class="nav-link" href="{{ url('/role') }}">
-                       <svg style="width: 15px; height: 13px;" xmlns="http://www.w3.org/2000/svg"
-                           viewBox="0 0 640 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
-                           <path fill="#ffffff"
-                               d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192l42.7 0c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0L21.3 320C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7l42.7 0C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3l-213.3 0zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352l117.3 0C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7l-330.7 0c-14.7 0-26.7-11.9-26.7-26.7z" />
-                       </svg>
+                       <i class="fas fa-user-cog"></i>
                        <span class="ml-1">Role</span>
                    </a>
                </li>
